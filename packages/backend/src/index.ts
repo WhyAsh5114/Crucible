4e905ea911e1edfbf6e24c893c9ded83791763cd
Jay Kerkar <bytewise0405@gmail.com>
2026-04-26 08:41:50 +0000

    feat: implement CORS middleware and update authentication route handling

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,23 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { auth } from './lib/auth';
 
 const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
+app.use(
+  '*',
+  cors({
+    maxAge: 600,
+    credentials: true,
+    origin: 'http://localhost:5173',
+    exposeHeaders: ['Content-Length'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
+
+app.on(['POST', 'GET'], '/api/auth/*', (c) => {
+  return auth.handler(c.req.raw);
 });
 
 export default app;
